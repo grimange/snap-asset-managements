09b53b7935311808d56f9c3d0bafa41005aed467
grimange <ramjfive24@gmail.com>
2025-03-07 06:35:25 +0800

    Add IP address extraction and save desktop data to the database
    Introduce a helper function to retrieve the user's IP address from request headers. Update the data processing to include the IP address and store desktop data using the `Desktop.objects.record` method.

--- remoteDesktops/views.py
+++ remoteDesktops/views.py
@@ -6,7 +6,16 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from remoteDesktops.models import Desktop
 
+def get_client_ip(request):
+    """Retrieve the real public IP address of the user."""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]  # Get the first IP in the list
+    else:
+        ip = request.META.get('REMOTE_ADDR')  # Fallback to direct IP
+    return ip
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -14,7 +23,8 @@ def receive_data(request):
     """Receives data from PowerShell"""
     try :
         data = request.data
-        print(data['motherboard'])
+        data['geo_location']['ip_address'] = get_client_ip(request)
+        Desktop.objects.record(request.data)
     except Exception as error:
         return Response({"error": str(error)}, status=400)
     return Response({"message": "Data received successfully"}, status=status.HTTP_200_OK)
