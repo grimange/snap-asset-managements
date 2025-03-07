1d33ddc8995117c1c12a0a10aa09719d35097bd2
grimange <ramjfive24@gmail.com>
2025-03-08 06:21:44 +0800

    Add endpoint for receiving Chrome history data
    Introduce a new URL pattern that maps to the `receive_chrome_history` view. This enables handling and processing of Chrome history data submissions. Other existing endpoints remain unchanged.

--- remoteDesktops/urls.py
+++ remoteDesktops/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from remoteDesktops.views import receive_data, FileUploadView
+from remoteDesktops.views import receive_data, FileUploadView, receive_chrome_history
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('receive-data/', receive_data, name='receive_data'),
+    path('receive_chrome_history/', receive_chrome_history, name='receive_chrome_history'),
     path('upload-image/', FileUploadView.as_view(), name='upload-image'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
